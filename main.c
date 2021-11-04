4fce3ec0b49523c306f68ddeabe0f3634b436a7c
Timothy <tsit20@stuy.edu>
2021-11-04 11:41:34 -0400

    made code for linux

--- main.c
+++ main.c
@@ -10,17 +10,22 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]){
-  DIR *d;
+  DIR *d = 0;
   struct dirent *dent;
   struct stat file;
   int size = 0;
   char s[100];
-  d = opendir(argv[1]);
-  while (errno){
-    printf("No such directory or no input. Try again\n");
-    read(STDIN_FILENO, &s, sizeof(s)-1);
-    sscanf(s, "%s\n", &s);
-    d = opendir(s);
+  if (argc>1){
+  	d = opendir(argv[1]);
+  }
+  else{
+  	errno = 2;
+  }
+  while (d == 0){
+  	printf("No such directory or no input. Try again\n");
+  	read(STDIN_FILENO, &s, sizeof(s)-1);
+  	sscanf(s,"%s\n",s);
+  	d = opendir(s);
   }
   while (1){
     dent = readdir(d);
@@ -40,7 +45,7 @@ int main(int argc, char *argv[]){
     }
     stat(dent->d_name, &file);
     if (S_ISDIR(file.st_mode)){
-      printf("permission:%o\t size:%d\t name:%s\t %s", file.st_mode, file.st_size, dent->d_name, ctime(&(file.st_atime)));
+      printf("permission:%o\t size:%ld\t name:%s\t %s", file.st_mode, file.st_size, dent->d_name, ctime(&(file.st_atime)));
     }
   }
   rewinddir(d);
@@ -52,7 +57,7 @@ int main(int argc, char *argv[]){
     }
     stat(dent->d_name, &file);
     if (S_ISREG(file.st_mode)){
-      printf("permission:%o\t size:%d\t name:%s\t %s", file.st_mode, file.st_size, dent->d_name, ctime(&(file.st_atime)));
+      printf("permission:%o\t size:%ld\t name:%s\t %s", file.st_mode, file.st_size, dent->d_name, ctime(&(file.st_atime)));
     }
   }
   closedir(d);
