6ff99a485d0aecfccca9e432b1ca77586a2c8557
U-DESKTOP-JLB2FSD\cxue1 <tsit20@stuy.edu>
2021-11-03 21:51:24 -0400

    finished adding user input

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]){
+  DIR *d;
+  struct dirent *dent;
+  struct stat file;
+  int size = 0;
+  char s[100];
+  d = opendir(argv[1]);
+  while (errno){
+    printf("No such directory or no input. Try again\n");
+    read(STDIN_FILENO, &s, sizeof(s)-1);
+    sscanf(s, "%s\n", &s);
+    d = opendir(s);
+  }
+  while (1){
+    dent = readdir(d);
+    if (dent==0){
+      break;
+    }
+    stat(dent->d_name, &file);
+    size += file.st_size;
+  }
+  printf("total size: %d\n\n", size);
+  rewinddir(d);
+  printf("Directories:\n");
+  while (1){
+    dent = readdir(d);
+    if (dent==0){
+      break;
+    }
+    stat(dent->d_name, &file);
+    if (S_ISDIR(file.st_mode)){
+      printf("permission:%o\t size:%d\t name:%s\t %s", file.st_mode, file.st_size, dent->d_name, ctime(&(file.st_atime)));
+    }
+  }
+  rewinddir(d);
+  printf("\nFiles:\n");
+  while (1){
+    dent = readdir(d);
+    if (dent==0){
+      break;
+    }
+    stat(dent->d_name, &file);
+    if (S_ISREG(file.st_mode)){
+      printf("permission:%o\t size:%d\t name:%s\t %s", file.st_mode, file.st_size, dent->d_name, ctime(&(file.st_atime)));
+    }
+  }
+  closedir(d);
+  return 0;
+}
